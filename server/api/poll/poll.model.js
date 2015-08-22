81c1bd9c5f967a1b896b70783c73906e29a9bff1
wlbrough <wlbrough@gmail.com>
2015-08-22 17:31:46 +0000

    Wrote tests for Poll model

--- server/api/poll/poll.model.js
+++ server/api/poll/poll.model.js
@@ -4,9 +4,42 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var PollSchema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  name: { 
+    type: String, 
+    trim: true,
+    required: 'Poll Name cannot be blank'
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  created: {
+    type: Date,
+    default: Date.now()
+  },
+  answers: [
+    {
+      text: { 
+        type: String, 
+        trim: true,
+        required: 'Answer cannot be blank'
+      },
+      votes: { type: Number, default: 0 }
+    }
+  ]
 });
+
+var Poll = mongoose.model('Poll', PollSchema);
+
+Poll.schema.path('answers').validate(function(v) {
+  return v.length >= 2;
+}, 'You must provide at least two options');
+
+PollSchema.statics.mostRecent = function(cb) {
+  this.find({})
+      .sort('-created')
+      .limit(5)
+      .exec(cb);
+};
 
 module.exports = mongoose.model('Poll', PollSchema);
--- server/api/poll/poll.spec.js
+++ server/api/poll/poll.spec.js
@@ -3,6 +3,15 @@
 var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
+var User = require('../user/user.model');
+var Poll = require('./poll.model');
+
+var user = new User({
+  provider: 'local',
+  name: 'Fake User',
+  email: 'test@test.com',
+  password: 'password'
+});
 
 describe('GET /api/polls', function() {
 
@@ -17,4 +26,60 @@ describe('GET /api/polls', function() {
         done();
       });
   });
+});
+
+describe('POST /api/polls', function() {
+  
+  it('should save a new record', function(done) {
+    var poll = new Poll({
+      name: 'Which is a better movie?',
+      owner: user,
+      created: Date.now(),
+      answers: [
+        { text: 'Bladerunner', votes: 0 },
+        { text: 'Up', votes: 0 }
+      ]
+    });
+    poll.save(function(err, saved) {
+      should.not.exist(err);
+      done();
+    });
+  });
+  
+  it('should throw an error when the name is blank', function(done) {
+    var poll = new Poll({
+      owner: user,
+      created: Date.now(),
+      answers: [
+        { text: 'Bladerunner', votes: 0 },
+        { text: 'Up', votes: 0 }
+      ]
+    });
+    poll.save(function(err) {
+      should.exist(err);
+      err.errors.name.message.should.equal('Poll Name cannot be blank');
+      done();
+    });
+  });
+  
+  it('should throw an error if there are less than two answers', function(done) {
+    var poll = new Poll({
+      name: 'Which is a better movie?',
+      owner: user,
+      created: Date.now(),
+      answers: [
+        { text: 'Bladerunner', votes: 0 }
+      ]
+    });
+    poll.save(function(err) {
+      should.exist(err);
+      done();
+    });
+  });
+  
+  afterEach(function(done) {
+    Poll.remove().exec();
+    done();
+  })
+  
 });
