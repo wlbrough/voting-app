349e371dfb3f94633e9e98168c07760b575ed79d
wlbrough <wlbrough@gmail.com>
2015-10-01 21:01:16 +0000

    Reimplemented api authentication for Polls

--- server/api/poll/index.js
+++ server/api/poll/index.js
@@ -10,9 +10,9 @@ var router = express.Router();
 
 router.get('/', controller.index);
 router.get('/:id', controller.show);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy);
+router.post('/', auth.isAuthenticated(), controller.create);
+router.put('/:id', auth.isAuthenticated(), controller.update);
+router.patch('/:id', auth.isAuthenticated(), controller.update);
+router.delete('/:id', auth.isAuthenticated(), controller.destroy);
 
 module.exports = router;
