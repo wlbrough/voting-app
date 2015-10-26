ecb65c58b99228317fcd148a320b07a59ea98b90
wlbrough <wlbrough@gmail.com>
2015-10-26 00:48:32 +0000

    Rewrote poll controllers into single directory

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -15,7 +15,7 @@ angular.module('workspaceApp')
         })
         .then( function() {
           // Logged in, redirect to home
-          $location.path('/');
+          $location.path('/polls');
         })
         .catch( function(err) {
           $scope.errors.other = err.message;
